fe31bb72b499021f7f21e0d0525fc0c430bf1a4c
sarapalacio0115 <sarapalacio0115@gmail.com>
2025-11-04 14:29:05 -0500

    Subo tercer  punto del taller

--- main.js
+++ main.js
@@ -92,3 +92,56 @@ const desarrollador1 = new Desarrollador("Arya", 19, 2500, "JavaScript");
 console.log(`Empleado: ${empleado1.nombre}, Salario: ${empleado1.calcularSalario()}$`);
 console.log(`Gerente: ${gerente1.nombre}, Salario: ${gerente1.calcularSalario()}$`);
 console.log(`Desarrollador: ${desarrollador1.nombre}, Lenguaje: ${desarrollador1.lenguaje}, Salario: ${desarrollador1.calcularSalario()}$`);
+
+// 3
+
+
+class Producto {
+    constructor(nombre, precio, marca) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.marca = marca;
+    }
+
+    descripcion() {
+        return `Producto: ${this.nombre}, Marca: ${this.marca}, Precio: $${this.precio}`;
+    }
+}
+class Telefono extends Producto {
+    constructor(nombre, precio, marca, almacenamiento, ram) {
+        super(nombre, precio, marca);
+        this.almacenamiento = almacenamiento;
+        this.ram = ram;
+    }
+
+    descripcion() {
+        return `${super.descripcion()}, Almacenamiento: ${this.almacenamiento}GB, RAM: ${this.ram}GB`;
+    }
+}
+
+
+class Laptop extends Producto {
+    constructor(nombre, precio, marca, procesador, pulgadas) {
+        super(nombre, precio, marca);
+        this.procesador = procesador;
+        this.pulgadas = pulgadas;
+    }
+
+    descripcion() {
+        return `${super.descripcion()}, Procesador: ${this.procesador}, Pantalla: ${this.pulgadas}"`;
+    }
+}
+
+
+const telefono1 = new Telefono("Iphone 15 S24", 1200, "Iphone", 200, 10);
+const laptop1 = new Laptop("Galaxy J7", 2500, "Samsumg", 300, 16);
+
+
+console.log(telefono1.descripcion());
+console.log(laptop1.descripcion());
+
+
+/// 4
+
+
+/// 5
